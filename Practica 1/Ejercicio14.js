8bc20ba9c659749360be6b601cb05828671d5750
israelcalanicanaviri <israelcalanicanaviri@gmail.com>
2026-03-30 01:44:30 -0300

    Refactor conCallback to use new promise function

--- Practica 1/Ejercicio14.js
+++ Practica 1/Ejercicio14.js
@@ -1,7 +1,15 @@
+function promesaEjemplo() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Hola desde promesa");
+        }, 2000);
+    });
+}
+
 function conCallback(callback) {
     promesaEjemplo().then(resultado => callback(resultado));
 }
 
 conCallback((res) => {
-    console.log("Callback:", res);
+    console.log("Resultado:", res);
 });
